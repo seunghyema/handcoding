6769017ac4a2ad020f3c4b61b2a84e26c02118a9
seunghyema <seunghyema53@gmail.com>
2026-08-22 01:14:39 +0900

    타이머 추가

--- 2주차/typing.js
+++ 2주차/typing.js
@@ -8,14 +8,22 @@ let score = 0;
 const typingForm = document.querySelector(".typing-form");
 const wordInput = document.querySelector(".word-input");
 const scoreDisplay = document.querySelector(".score");
+const timerDisplay = document.querySelector('.timer');
+let timeLeft = 5;
+let timerId;
 
 function startGame() {
+  score = 0;
+  scoreDisplay.textContent = '점수는 ' + score;
+  wordInput.value = '';
   startScreen.classList.add("hidden");
   gameScreen.classList.remove("hidden");
   statusBar.classList.remove("hidden");
   currentWord = pickWord();
   targetWord.textContent = currentWord;
+  startTimer();
 }
+
 startButton.addEventListener("click", startGame);
 
 const words = [
@@ -43,9 +51,39 @@ function checkWord(event) {
     wordInput.value = "";
     currentWord = pickWord();
     targetWord.textContent = currentWord;
+    startTimer();
   } else {
     wordInput.value = "";
   }
 }
 
 typingForm.addEventListener("submit", checkWord);
+
+function gameOver(){
+  startScreen.classList.remove("hidden");
+  gameScreen.classList.add("hidden");
+  statusBar.classList.add("hidden");
+
+}
+
+function tick(){
+  timeLeft -= 1;
+  timerDisplay.textContent = timeLeft;
+  if (timeLeft === 0){
+    clearInterval(timerId);
+    gameOver();
+  }
+
+ 
+}
+
+function startTimer(){
+
+  if(timerId){
+    clearInterval(timerId);
+  }
+  timeLeft = 5;
+  timerDisplay.textContent = timeLeft;
+  timerId = setInterval(tick,1000);
+  
+}
